2fa3c4ee29108bad2fb5ee880d510a070aec2411
Admin <Admin@KOSCOMK-M189TAI>
2023-12-14 22:39:14 +0900

    Commit in devB branch(revised A.java, B.java)

--- MyGit/src/assGit/A.java
+++ MyGit/src/assGit/A.java
@@ -4,5 +4,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("This is Git Assignment!");
 		System.out.println("This is the revised version of A.java.");
+		B name = new B();
+		name.sayMyName("WonYuRim");
 	}
 }
--- MyGit/src/assGit/B.java
+++ MyGit/src/assGit/B.java
@@ -3,5 +3,6 @@ package assGit;
 public class B {
 	public void sayMyName(String name) {
 		System.out.println("My name is " + name);
+		System.out.println("What is your name?(dev B revised version)");
 	}
 }
