ce7b3fd6a8796f4300241fd90facdccae8cae1cf
Admin <Admin@KOSCOMK-M189TAI>
2023-12-14 22:44:55 +0900

    Change in master branch(A.java, B.java)

--- MyGit/src/assGit/A.java
+++ MyGit/src/assGit/A.java
@@ -4,5 +4,6 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("This is Git Assignment!");
 		System.out.println("This is the revised version of A.java.");
+		System.out.println("A changed in master branch.");
 	}
 }
--- MyGit/src/assGit/B.java
+++ MyGit/src/assGit/B.java
@@ -3,5 +3,6 @@ package assGit;
 public class B {
 	public void sayMyName(String name) {
 		System.out.println("My name is " + name);
+		System.out.println("B changed in master branch");
 	}
 }
